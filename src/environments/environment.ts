ccf05225ab218bd415523a253f4e94ac5453999f
Emily <emily-ss@hotmail.com>
2025-08-18 17:39:36 -0300

    ajuste na variáveis de ambiente

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,4 @@ export const environment = {
   production: false,
   supabaseUrl: 'https://jmuscgoyoutsfgekcuel.supabase.co',
   supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImptdXNjZ295b3V0c2ZnZWtjdWVsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTU0NjY1MzIsImV4cCI6MjA3MTA0MjUzMn0.ZT1vpA1V6cLJKTe1f7J0fxzeXRC2C4HjlCRYUsn2XBY',
-  stripePublishableKey: 'pk_test_xxx'
 };
