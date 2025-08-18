cf0990fdfb2cfb6e92558a24ead967cae533f623
Emily <emily-ss@hotmail.com>
2025-08-18 17:31:17 -0300

    ajuste nas variáveis de ambiente

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,6 @@
 export const environment = {
   production: true,
-  supabaseUrl: (import.meta as any).env['NG_APP_SUPABASE_URL'] ?? '',
-  supabaseAnonKey: (import.meta as any).env['NG_APP_SUPABASE_ANON_KEY'] ?? '',
-  stripePublishableKey: (import.meta as any).env['NG_APP_STRIPE_KEY'] ?? ''
+  supabaseUrl: (import.meta as any).env?.VITE_SUPABASE_URL ?? '',
+  supabaseAnonKey: (import.meta as any).env?.VITE_SUPABASE_ANON_KEY ?? '',
 };
 
